9ac86d61ddca4d249b55c52c53e13da34d4297a2
JDO <jdoucetOC@gmail.com>
2021-03-12 00:26:22 +0100

    Update View, Controller, DbManage

--- controller.py
+++ controller.py
@@ -1,11 +1,45 @@
 import classes
+import dbmanage
 
 class Controller:
-    def __init__(self):
+    def __init__(self, view):
+        self.tournament = ''
+        self.view = view
+        self.db = dbmanage.DbManager()
+        self.players = []
+
+    def run(self):
+        choice = self.view.menu()
+        if choice == "a":
+            self.new_tt()
+        if choice == "b":
+            self.resumt_tt()
+        if choice == "c":
+            self.edit_players()
+        if choice == "d":
+            self.reports()
+
+    def new_tt(self):
+        name, place, date, timetype, desc = self.view.new_tournament()
+        self.tournament = classes.Tournament(name, place, date, timetype, desc)
+        players = self.view.add_players()
+        if players == "a":
+            self.demo_players()
+        if players == "b":
+            self.pick_players()
+
+    def demo_players(self):
+        self.players = self.db.return_demo()
+        self.view.show_pname(self.players)
+
+    def pick_players(self):
         pass
 
-    def new_tournament(self):
+    def resume_tt(self):
         pass
 
-    def resume_tournament(self):
+    def edit_players(self):
+        pass
+
+    def reports(self):
         pass
--- views.py
+++ views.py
@@ -1,42 +1,52 @@
-import controller
-import dbmanage
-
-
 class Views:
     def __init__(self):
-        self.control = controller.Controller()
-        self.managedb = dbmanage.DbManager
+        pass
 
-    def menu(self):
-        print('Chess Tournament Menu :')
+    @staticmethod
+    def menu():
+        print('Chess Tournament Menu :\n')
         choice = input('A: New Tournament\n'
                        'B: Resume Tournament\n'
                        'C: Edit Players\n'
                        'D: Reports\n').lower()
 
-        if choice == "a":
-            self.new_tournament()
-        if choice == "b":
-            self.resume_tournament()
-        if choice == "c":
-            self.edit_players()
-        if choice == "d":
-            self.reports()
+        return choice
 
-    def new_tournament(self):
-        print("New tournament :")
-        self.control.new_tournament()
+    @staticmethod
+    def new_tournament():
+        print("\n\nNew tournament, enter tournament attributes")
+        name = input("Tournament name : ")
+        place = input("City : ")
+        date = input("Date : ")
+        timetype = input("TimeType : ")
+        desc = input("Description : ")
+        return name, place, date, timetype, desc
 
-    def resume_tournament(self):
+    @staticmethod
+    def add_players():
+        print("\n\nAdd players")
+        players = input("A: Add 8 Pre-selected Player ( demo )\n"
+                        "B: Pick 8 Players\n").lower()
+        return players
+
+    @staticmethod
+    def show_pname(players):
+        for player in players:
+            print(player.lastName)
+
+    @staticmethod
+    def resume_tournament():
         print("Resuming previous tournament...")
-        self.control.resume_tournament()
+        pass
 
-    def edit_players(self):
+    @staticmethod
+    def edit_players():
         print("Edit players :")
-        self.managedb.modify_elo()
+        pass
 
-    def reports(self):
+    @staticmethod
+    def reports():
         print("Reports :")
-        self.managedb.reports()
+        pass
 
 
